9f9c7532c105936f3265178db967be69cfea4600
Joshua Minien <132277561+aerojam95@users.noreply.github.com>
2025-02-22 16:48:50 +0000

    main: logger tests

--- src/custom_logger.py
+++ src/custom_logger.py
@@ -1,18 +1,19 @@
-#=============================================================================
+# =============================================================================
 # Modules
-#=============================================================================
+# =============================================================================
 
-# Python in built modules
+# Python modules
 import logging.config
 
 # Third party modules
 import yaml
 
-#=============================================================================
+# =============================================================================
 # Functions
-#=============================================================================
+# =============================================================================
 
-def get_logger(yaml_config_file_path:str):
+
+def get_logger(yaml_config_file_path: str):
     """Set up logger based on configuration from a YAML file
 
     Args:
@@ -26,25 +27,23 @@ def get_logger(yaml_config_file_path:str):
         # Load logging configuration from YAML file
         with open(yaml_config_file_path, "r") as file:
             config = yaml.safe_load(file)
-        
+
         # Apply the logging configuration
         logging.config.dictConfig(config["logging"])
-        
+
         # Dynamically get the logger's name from YAML configuration
-        logger_name = config["logging"]["loggers"].get("logger_name",
-            "default_logger")
-        
+        logger_name =next(iter(config["logging"]["loggers"]), "default_logger")
+
         # Use the logger name dynamically
         logger = logging.getLogger(logger_name)
         return logger
-    
+
     except FileNotFoundError as e:
         print(f"Error: The logging configuration file was not found: {e}")
         raise
 
     except yaml.YAMLError as e:
-        print(f"Error: There was an issue parsing the YAML configuration
-            file: {e}")
+        print(f"Error: There was an issue parsing the YAML configuration file: {e}")
         raise
 
     except Exception as e:
